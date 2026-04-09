3f3326dc7f8232be633e40b521e08ebfa76aab19
Handroc <andricassani42069@gmail.com>
2026-04-09 15:43:32 +0200

    fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import asyncio
+from bisect import insort
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = []
-    for coro in asyncio.as_completed(tasks):
-        delay = await coro
-        delays.append(delay)
-    return sorted(delays)
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        insort(delays, delay)
+    return delays
